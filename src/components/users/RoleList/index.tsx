4ecd9d9422b646d22c41e01562969048d84f202b
Emmanuel tabares <emanuel_tabares@hotmail.com>
2024-10-06 21:14:14 -0500

    a

--- src/components/users/RoleList/index.tsx
+++ src/components/users/RoleList/index.tsx
@@ -1,30 +1,27 @@
 "use client"
 
-import { useAxios } from "@/hooks/useAxios"
-import { useEffect, useState } from "react"
-
 export const RolesList = () => {
 
-    const { get, response, isLoading } = useAxios()
-    const [roles, setRoles] = useState<unknown[]>()
-    useEffect(() => {
-        get('roles/?page=1&items=100')
-    }, []) // eslint-disable-next-line react-hooks/exhaustive-deps
+    // const { get, response, isLoading } = useAxios()
+    // const [roles, setRoles] = useState<unknown[]>()
+    // useEffect(() => {
+    //     get('roles/?page=1&items=100')
+    // }, []) // eslint-disable-next-line react-hooks/exhaustive-deps
 
-    useEffect(() => {
-        setRoles(response?.info?.roles as unknown[])
-    }, [response])
+    // useEffect(() => {
+    //     setRoles(response?.info?.roles as unknown[])
+    // }, [response])
 
     return (
         <div>
             <h6>Role list</h6>
 
-            {isLoading ? <h1>Cargando...</h1> :
+            {/* {isLoading ? <h1>Cargando...</h1> :
                 roles?.map((role: unknown) => (
 
                     <p>{role?.name}</p>
 
-                ))}
+                ))} */}
 
         </div>
     )
